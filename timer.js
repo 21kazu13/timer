ca32e459ab6bf58dc6c10ba5455d9ae69e7affde
21kazu13 <0107kazu@gmail.com>
2019-06-16 15:46:03 +0900

    cleaning done

--- timer.js
+++ timer.js
@@ -5,10 +5,6 @@ const defaultpts = 6800;
 const defaultmsg = "00'00\"00";
 document.getElementById("showArea").innerHTML = defaultmsg;
 
-var sumtofile = [];
-var msgflag = -1; //0 -> encourge, 1 -> best, 2 -> worst, 3 -> summery of hours
-var recflag; //0 -> not record, 1 -> best lap, -1 -> worst lap
-
 //keydown code
 document.onkeydown = keydown;
 //when onkeydown event occur, function "keydown" is executed.
@@ -29,10 +25,20 @@ function keydown(){
         .then(displayLap)
         .then(checkLap);
     }else if(event.keyCode == 77){//m
-        displaymessage();
+        switchMessage();
     }
 }
 //keydown end
+
+//convert time to string code
+function convertTime(lapinmillisec){
+    var sec100 = Math.floor((lapinmillisec % 1000 )/ 10);
+    var sec = parseInt((lapinmillisec / 1000) % 60, 10);
+    var min = parseInt((lapinmillisec / (1000 * 60)) % 60, 10);
+    var msg = addzero(min) + "'" + addzero(sec) + "\"" + addzero(sec100);
+    return msg;
+}
+//convert time to string end
 
 //addzero code
 function addzero(num){
@@ -75,11 +81,7 @@ function start(){
 //show laptime code
 function showSec(time){
     var diff = new Date().getTime() - time;
-    var sec100 = Math.floor((diff % 1000 )/ 10);
-    var sec = parseInt((diff / 1000) % 60, 10);
-    var min = parseInt((diff / (1000 * 60)) % 60, 10);
-
-    var msg = addzero(min) + "'" + addzero(sec) + "\"" + addzero(sec100);
+    var msg = convertTime(diff);
     document.getElementById("showArea").innerHTML = msg;
 }
 //show laptime end
@@ -87,8 +89,7 @@ function showSec(time){
 //show nowTime start
 function showTimeNow(){
     var nowTime = new Date();
-    var datemsg = nowTime.toLocaleString();
-    document.getElementById("dateArea").innerHTML = datemsg;
+    document.getElementById("dateArea").innerHTML = nowTime.toLocaleString();
     /*
     if(nowTime.getMinutes() === 59 && sumflag === 0){
         myNS.flags.sumflag = 1;
@@ -96,7 +97,10 @@ function showTimeNow(){
         Promise.resolve(hourSummeryPrePare(myNS.params.runhour))
         .then(function(hoursum){
             myNS.recordinhour.push(hoursum);
-        });
+            return hoursum;
+        })
+        .then(hourSummeryDisplay)
+        .then(displayMessage);
     }else if(nowTime.getMinutes() === 0 && sumflag === 1){
         myNS.flags.sumflag = 0;
         myNS.params.misscount = [0,0,0];
@@ -110,7 +114,10 @@ function showTimeNow(){
         Promise.resolve(hourSummeryPrePare(myNS.params.runhour))
         .then(function(hoursum){
             myNS.recordinhour.push(hoursum);
-        });
+            return hoursum;
+        })
+        .then(hourSummeryDisplay)
+        .then(displayMessage);
     }else if(nowTime.getSeconds() === 2 && myNS.flags.sumflag === 1){
         myNS.flags.sumflag = 0;
         myNS.params.misscount = [0,0,0];
@@ -135,6 +142,24 @@ function hourSummeryPrePare(thishour){
 }
 //summerize 60min end
 
+//display summery code
+function hourSummeryDisplay(sum){
+    if(sum[0] > 0){
+        var diff = sum[2];
+        var lapmsg = convertTime(diff*1000);
+        var nowTime = new Date();
+        var msg = nowTime.getHours()+ "時までの1時間まとめ<br>  平均Lap : " + lapmsg + "<br>";
+        msg += "  時速 : "+ sum[1] + "pts/H<br>"
+        msg += "  誤答 : " +sum[3]+ "回<br>立ち回りミス : " +sum[4]+ "回<br>リタイア : " +sum[5]+ "回";
+    }else{
+        msg = "No running in previous hour!"
+    }
+    console.log(msg);
+    myNS.flags.msgflag = 0;
+    return msg
+}
+//display summery end
+
 // stop timer code
 function stop(){
     if(window.confirm('Are you sure to stop the timer?')){
@@ -154,8 +179,7 @@ function saveornot(){
     if(window.confirm('Do you want to save these records?')){
         // https://www.indetail.co.jp/blog/12150/
         var csv = [];
-        myNS.record.forEach(function (time, index) {
-            //csv.push(index + ',' + time);
+        myNS.record.forEach(function (time, index){
             csv.push((index+1) + ',' + time.join(','));// for the case myNS.record include array(for hour flag etc)
         });
         console.log(csv);
@@ -165,7 +189,18 @@ function saveornot(){
         document.body.appendChild(element);
         element.click();
         document.body.removeChild(element);
-        //時間毎のさまりmyNS.recordinhourも入れたい
+
+        var csv2 = [];
+        myNS.recordinhour.forEach(function (time, index){
+            csv2.push((index) + ',' + time.join(','));// for the case myNS.record include array(for hour flag etc)
+        });
+        console.log(csv2);
+        var element2 = document.createElement('a');
+        element2.href = 'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURIComponent(csv2.join('\n'));
+        element2.setAttribute('download', 'sumdata.csv');
+        document.body.appendChild(element2);
+        element2.click();
+        document.body.removeChild(element2);
     }
 }
 //save records end
@@ -182,7 +217,6 @@ function reset(){
     myNS = {
         flags : {}
     };
-    msgflag = -1;
 }
 //reset timer end
 
@@ -194,52 +228,52 @@ function lap(){
     var difflap = diff/1000.0;//laptime in sec
     myNS.params.launchTime = lapTime;//for next calculation
     myNS.record.push([difflap,myNS.params.runhour]);
-//    calculateMeanLap(difflap,myNS.info.lapnum);
     return [diff,myNS.info.lapnum];
 }
 //calculate laptime end
 
-/*
-//calculate meanlap code
-function calculateMeanLap(presentlap, lapnum){
-    myNS.info.meanlap = Math.round(((lapnum-1) * myNS.info.meanlap + presentlap)/lapnum*1000)/1000;
-    console.log(myNS.info.meanlap);
-}
-//calculate meanlap end
-*/
-
 //display laptime code
 function displayLap(param){
-    var diff = param[0];
-    var lapnum = param[1];
-    var sec100 = Math.floor((diff % 1000 )/ 10);
-    var sec = parseInt((diff / 1000) % 60, 10);
-    var min = parseInt((diff / (1000 * 60)) % 60, 10);
-    var lapmsg = addzero(min) + "'" + addzero(sec) + "\""+ addzero(sec100);
-
+    var lapmsg = convertTime(param[0]);
     var lapArea = document.getElementById("showLapArea");
     var makeli = document.createElement("li");
-    makeli.innerHTML = "LAP "+ lapnum + " : " +lapmsg;
+    makeli.innerHTML = "LAP "+ param[1] + " : " +lapmsg;
     lapArea.append(makeli);
     lapArea.scrollTo(0, makeli.offsetTop);
-    return diff/1000.0;
+    return param[0]/1000.0;
 }
 //display laptime end
 
 //check laptime (best/worst) code
-function checkLap(thislap){
+function checkLap(thislap){//in second
+    var newrecflag = 0;
     if(myNS.info.lapnum === 1){
         myNS.info.bestlap = thislap;
         myNS.info.worstlap = thislap;
     }else{
         if(myNS.info.bestlap > thislap){
             myNS.info.bestlap = thislap;
+            newrecflag = 1;
         }else if(myNS.info.worstlap < thislap){
             myNS.info.worstlap = thislap;
+            newrecflag = 1;
         }
-    }                   
+    }
+    if(newrecflag === 1){
+        Promise.resolve(displayRecord())
+        .then(displayMessage);
+    }
 }
 //check laptime end
+
+//display record code
+function displayRecord(){
+    var msg = "Best Laptime  : "+convertTime(myNS.info.bestlap*1000)+"<br>";
+    msg += "Worst Laptime : " + convertTime(myNS.info.worstlap*1000);
+    myNS.flags.msgflag = 1;
+    return msg;
+}
+//display record end
 
 //edit lap code
 function editLap(){
@@ -252,7 +286,7 @@ function editLap(){
     var editlap = parsePromptMsg('where to edit?');
     var howmanylap = parsePromptMsg('how many lost your laps?')+1;
 
-    if(editlap > 0 && editlap < myNS.record.length+1 && howmanylap > 0){
+    if(editlap > 0 && editlap < myNS.record.length+1 && howmanylap > 1){
         var newlap = Math.round(myNS.record[editlap-1][0]/howmanylap*1000)/1000;
         for(var i=0;i<howmanylap;i++){
             var newrec = [newlap,myNS.record[editlap-1][1]];
@@ -262,8 +296,8 @@ function editLap(){
         myNS.info.lapnum += (howmanylap-1);
 
         editDisplay();
-        if(myNS.record[el-1][1] < myNS.params.runhour){
-            editSummery(editlap);
+        if(myNS.record[editlap-1][1] < myNS.params.runhour){
+            editSummery();
         }    
     }
 }
@@ -284,7 +318,7 @@ function editDisplay(){
 //edit display end
 
 //edit hour summery code
-function editSummery(el){
+function editSummery(){
     myNS.recordinhour.forEach((val,index) => {
         Promise.resolve(hourSummeryPrePare(index+1))
         .then(function(newsum){
@@ -296,121 +330,22 @@ function editSummery(el){
 }
 //edit hour summery end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//残りメッセージ類だけ
-//とりあえず上と分離
-function lapmessage(){
-    if(recflag === 1){
-        msgflag = 1;
-        createmessage(msgflag);
-        var congmsg = document.createElement("p");
-        congmsg.className = "onetime";
-        $(".onetime").remove();
-        congmsg.innerHTML = "おめでとう！best lap更新だよ！"
-        $("#messageArea").append(congmsg);
-        document.getElementsByClassName("topyachiyo")[0].src = "yachiyo/201503_yachiyo_happy.png";
-    }else if(recflag === -1){
-        msgflag = 2;
-        createmessage(msgflag);
-        document.getElementsByClassName("topyachiyo")[0].src = "yachiyo/201503_yachiyo_sad.png";
-        var consmsg = document.createElement("p");
-        consmsg.className = "onetime";
-        $(".onetime").remove();
-        consmsg.innerHTML = "残念...worst lapだよ...引きずらないで次頑張れ！"
-        $("#messageArea").append(consmsg);
-    }else{
-        $(".onetime").remove();
-        document.getElementsByClassName("topyachiyo")[0].src = "yachiyo/201503_yachiyo_surprise.png";        
-    }
-}
-
-
-
-//set message from yachiyo start
-function displaymessage(){
-    msgflag++;
-    if(msgflag === 4){msgflag = 0;}
-    createmessage(msgflag);
-}
-document.getElementById("imgArea").addEventListener("click", displaymessage);
-
-function createmessage(int){
-    document.getElementsByClassName("topyachiyo")[0].src = "yachiyo/201503_yachiyo_surprise.png";
+//display message code
+function displayMessage(msg){
     var message = document.createElement("p");
     $("#messageArea").empty();
-    if(int === 0){
-        message.innerHTML = "あなたの力には、期待しているから。";
-        message.style.textAlign = "right";
-        $("#messageArea").append(message);
-    }else if(int === 1){
-        message.innerHTML = "Best lap : " + displayBestLap();
-        $("#messageArea").append(message);
-    }else if(int === 2){
-        message.innerHTML = "Worst lap : " + displayWorstLap();
-        $("#messageArea").append(message);
-    }else if(int === 3){
-        message.innerHTML = hourSummeryDisplay();
-        $("#messageArea").append(message);
-    }
+    message.innerHTML = msg;
+    $("#messageArea").append(message);
 }
-//set message from yachiyo end
+//display message end
 
-//prepare to display best/worst lap start
-function displayBestLap(){
-    if(document.getElementsByClassName("bestLap")[0]){
-        return document.getElementsByClassName("bestLap")[0].innerText.split(": ")[1];
+//switch message code
+function switchMessage(){
+    var switchmsg;
+    if(myNS.flags.msgflag === 0){
+        switchmsg = displayRecord();
     }else{
-        return "";
+        switchmsg = hourSummeryDisplay(myNS.recordinhour[myNS.params.runhour-1])
     }
+    displayMessage(switchmsg);
 }
-function displayWorstLap(){
-    if(document.getElementsByClassName("worstLap")[0]){
-        return document.getElementsByClassName("worstLap")[0].innerText.split(": ")[1];
-    }else{
-        return "";
-    }
-}
-//prepare to display best/worst lap end
-
-//prepare and display an hour summery start周回数、時速、平均、３ボタン
-function hourSummeryDisplay(){
-    var i = sumtofile.length - 1;
-    if(sumtofile[i][2]){
-        var sec100 = parseInt((String(sumtofile[i][2]).split(".")[1]).slice(0,2));
-        var sec = parseInt(sumtofile[i][2])%60;
-        var min = (parseInt(sumtofile[i][2]) - sec)/60;
-        var msg = i + "時-" + (i+1) + "時まとめ<br>平均Lap : " + min + "'" + addzero(sec) + "\"" + addzero(sec100) + "<br>";
-        msg += "誤答 : " + "回 立ち回りミス : " + "回 リタイア : " + "回<br>";
-        msg += "時速 : "+ sumtofile[i][1] + "pts/hour"     
-    }else{
-        msg = "No running in previous hour!"
-    }
-    return msg
-}
-//prepare and display an hour summery end
-
-//saveornot の中の処理でできるため以下不要
-/*
-//get laptimelist start
-function download(data){
-    var blob = new Blob(data, {type: "text/plain"});
-    var a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.target = '_blank';
-    a.download = 'test.dat';
-    a.click();
-}
-//get laptimelist end
-*/
